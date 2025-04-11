9c4d1f3637248a9d697c07a976f6fcd2f1b21306
WheatFarmerMC <simonrdudek@gmail.com>
2025-04-10 19:34:40 -0500

    cds

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,78 +1,76 @@
-const listingTitleField = document.querySelector('.title');
-const articleFeild = document.querySelector('.article');
+import { db } from './firebase.js';
 
-// banner
-const bannerImage = document.querySelector('#banner-upload');
-const banner = document.querySelector(".banner");
-let bannerPath;
+document.addEventListener("DOMContentLoaded", () => {
+    const listingTitleField = document.querySelector('.title');
+    const articleFeild = document.querySelector('.article');
+    const bannerImage = document.querySelector('#banner-upload');
+    const banner = document.querySelector(".banner");
+    const publishBtn = document.querySelector('.publish-btn');
+    const uploadInput = document.querySelector('#image-upload');
 
-const publishBtn = document.querySelector('.publish-btn');
-const uploadInput = document.querySelector('#image-upload');
+    let bannerPath;
 
-bannerImage.addEventListener('change', () => {
-    uploadImage(bannerImage, "banner");
-})
+    bannerImage.addEventListener('change', () => {
+        uploadImage(bannerImage, "banner");
+    });
 
-uploadInput.addEventListener('change', () => {
-    uploadImage(uploadInput, "image");
-})
-const uploadImage = (uploadFile, uploadType) => {
-    const [file] = uploadFile.files;
-    if(file && file.type.includes("image")){
-        const formdata = new FormData();
-        formdata.append('image', file);
+    uploadInput.addEventListener('change', () => {
+        uploadImage(uploadInput, "image");
+    });
 
-        fetch('/upload', {
-            method: 'post',
-            body: formdata
-        }).then(res => res.json())
-        .then(data => {
-            if(uploadType == "image"){
-                addImage(data, file.name);
-            } else{
-                bannerPath = `${location.origin}/${data}`;
-                banner.style.backgroundImage = `url("${bannerPath}")`;
-            }
-        })
-    } else{
-        alert("upload Image only");
-    }
-}
+    const uploadImage = (uploadFile, uploadType) => {
+        const [file] = uploadFile.files;
+        if(file && file.type.includes("image")){
+            const formdata = new FormData();
+            formdata.append('image', file);
 
-
-const addImage = (imagepath, alt) => {
-    let curPos = articleFeild.selectionStart;
-    let textToInsert = `\r![${alt}](${imagepath})\r`;
-    articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
-}
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-publishBtn.addEventListener('click', () => {
-    if(articleFeild.value.length && listingTitleField.value.length){
-        // generating id
-        let letters = 'abcdefghijklmnopqrstuvwxyz';
-        let listingTitle = listingTitleField.value.split(" ").join("-");
-        let id = '';
-        for(let i = 0; i < 4; i++){
-            id += letters[Math.floor(Math.random() * letters.length)];
+            fetch('/upload', {
+                method: 'post',
+                body: formdata
+            }).then(res => res.json())
+            .then(data => {
+                if(uploadType === "image"){
+                    addImage(data, file.name);
+                } else {
+                    bannerPath = `${location.origin}/${data}`;
+                    banner.style.backgroundImage = `url("${bannerPath}")`;
+                }
+            });
+        } else {
+            alert("upload Image only");
         }
+    };
 
-        // setting up docName
-        let docName = `${listingTitle}-${id}`;
-        let date = new Date(); // for published at info
+    const addImage = (imagepath, alt) => {
+        let curPos = articleFeild.selectionStart;
+        let textToInsert = `\r![${alt}](${imagepath})\r`;
+        articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
+    };
 
-        //access firstore with db variable;
-        db.collection("listings").doc(docName).set({
-            title: listingTitleField.value,
-            article: articleFeild.value,
-            bannerImage: bannerPath,
-            publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
-        })
-        .then(() => {
-            location.href = `/${docName}`;
-        })
-        .catch((err) => {
-            console.error(err);
-        })
-    }
-})
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    publishBtn.addEventListener('click', () => {
+        if(articleFeild.value.length && listingTitleField.value.length){
+            let letters = 'abcdefghijklmnopqrstuvwxyz';
+            let listingTitle = listingTitleField.value.split(" ").join("-");
+            let id = '';
+            for(let i = 0; i < 4; i++){
+                id += letters[Math.floor(Math.random() * letters.length)];
+            }
+
+            let docName = `${listingTitle}-${id}`;
+            let date = new Date();
+
+            db.collection("listings").doc(docName).set({
+                title: listingTitleField.value,
+                article: articleFeild.value,
+                bannerImage: bannerPath,
+                publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+            }).then(() => {
+                location.href = `/${docName}`;
+            }).catch((err) => {
+                console.error(err);
+            });
+        }
+    });
+});
