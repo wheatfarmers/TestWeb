3926fc3c63f68b5eb0c4a91ecd5959d7a45545c1
WheatFarmerMC <simonrdudek@gmail.com>
2025-05-06 21:15:27 -0500

    fds

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,75 +1,79 @@
+const listingTitleField = document.querySelector('.title');
+const articleFeild = document.querySelector('.article');
 
-document.addEventListener("DOMContentLoaded", () => {
-    const listingTitleField = document.querySelector('.title');
-    const articleFeild = document.querySelector('.article');
-    const bannerImage = document.querySelector('#banner-upload');
-    const banner = document.querySelector(".banner");
-    const publishBtn = document.querySelector('.publish-btn');
-    const uploadInput = document.querySelector('#image-upload');
+// banner
+const bannerImage = document.querySelector('#banner-upload');
+const banner = document.querySelector(".banner");
+let bannerPath;
 
-    let bannerPath;
+const publishBtn = document.querySelector('.publish-btn');
+const uploadInput = document.querySelector('#image-upload');
 
-    bannerImage.addEventListener('change', () => {
-        uploadImage(bannerImage, "banner");
-    });
 
-    uploadInput.addEventListener('change', () => {
-        uploadImage(uploadInput, "image");
-    });
+const uploadImage = (uploadFile, uploadType) => {
+    const [file] = uploadFile.files;
+    if(file && file.type.includes("image")){
+        const formdata = new FormData();
+        formdata.append('image', file);
 
-    const uploadImage = (uploadFile, uploadType) => {
-        const [file] = uploadFile.files;
-        if(file && file.type.includes("image")){
-            const formdata = new FormData();
-            formdata.append('image', file);
-
-            fetch('/upload', {
-                method: 'post',
-                body: formdata
-            }).then(res => res.json())
-            .then(data => {
-                if(uploadType === "image"){
-                    addImage(data, file.name);
-                } else {
-                    bannerPath = `${location.origin}/${data}`;
-                    banner.style.backgroundImage = `url("${bannerPath}")`;
-                }
-            });
-        } else {
-            alert("upload Image only");
-        }
-    };
-
-    const addImage = (imagepath, alt) => {
-        let curPos = articleFeild.selectionStart;
-        let textToInsert = `\r![${alt}](${imagepath})\r`;
-        articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
-    };
-
-    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-    publishBtn.addEventListener('click', () => {
-        if(articleFeild.value.length && listingTitleField.value.length){
-            let letters = 'abcdefghijklmnopqrstuvwxyz';
-            let listingTitle = listingTitleField.value.split(" ").join("-");
-            let id = '';
-            for(let i = 0; i < 4; i++){
-                id += letters[Math.floor(Math.random() * letters.length)];
+        fetch('/upload', {
+            method: 'post',
+            body: formdata
+        }).then(res => res.json())
+        .then(data => {
+            if(uploadType == "image"){
+                addImage(data, file.name);
+            } else{
+                bannerPath = `${location.origin}/${data}`;
+                banner.style.backgroundImage = `url("${bannerPath}")`;
             }
+        })
+    } else{
+        alert("upload Image only");
+    }
+}
+bannerImage.addEventListener('change', () => {
+    uploadImage(bannerImage, "banner");
+})
 
-            let docName = `${listingTitle}-${id}`;
-            let date = new Date();
+uploadInput.addEventListener('change', () => {
+    uploadImage(uploadInput, "image");
+})
 
-            db.collection("listings").doc(docName).set({
-                title: listingTitleField.value,
-                article: articleFeild.value,
-                bannerImage: bannerPath,
-                publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
-            }).then(() => {
-                location.href = `/${docName}`;
-            }).catch((err) => {
-                console.error(err);
-            });
+
+const addImage = (imagepath, alt) => {
+    let curPos = articleFeild.selectionStart;
+    let textToInsert = `\r![${alt}](${imagepath})\r`;
+    articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
+}
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+publishBtn.addEventListener('click', () => {
+    if(articleFeild.value.length && listingTitleField.value.length){
+        // generating id
+        let letters = 'abcdefghijklmnopqrstuvwxyz';
+        let listingTitle = listingTitleField.value.split(" ").join("-");
+        let id = '';
+        for(let i = 0; i < 4; i++){
+            id += letters[Math.floor(Math.random() * letters.length)];
         }
-    });
-});
+
+        // setting up docName
+        let docName = `${listingTitle}-${id}`;
+        let date = new Date(); // for published at info
+
+        //access firstore with db variable;
+        db.collection("listings").doc(docName).set({
+            title: listingTitleField.value,
+            article: articleFeild.value,
+            bannerImage: bannerPath,
+            publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+        })
+        .then(() => {
+            location.href = `/${docName}`;
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
+})
--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBSvV3l7I-sjxgl2RhW7chZdNJf5DJEHO8",
   authDomain: "webtest-191fb.firebaseapp.com",
@@ -15,7 +10,6 @@ const firebaseConfig = {
   appId: "1:361311676080:web:18c86e37105a70062fbd75",
   measurementId: "G-PLZ7K0RWS6"
 };
+firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let db = firebase.firestore();
